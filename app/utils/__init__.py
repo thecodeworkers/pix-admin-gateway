caa26ba15543b5a29030cd4d8e9d30955904fd3a
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-10-09 09:50:29 -0400

    key manager util

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,2 +1,2 @@
 from .grpc_error import message_error
-from .key_generator import generation, generate_app_keys, verify_signature
+from .key_manager import generation, generate_app_keys, verify_signature
